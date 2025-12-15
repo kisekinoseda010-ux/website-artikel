f6dd58b0224c51de48309c60302f2cb979915582
kisekinoseda010-ux <kisekinoseda010@gmail.com>
2025-12-15 08:30:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("articles");
+
+  fetch("trends.json")
+    .then(res => {
+      if (!res.ok) throw new Error("JSON tidak ditemukan");
+      return res.json();
+    })
+    .then(data => {
+      container.innerHTML = "";
+
+      data.forEach(item => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+          <img src="https://via.placeholder.com/300x450?text=${encodeURIComponent(item.keyword)}">
+          <h3>${item.keyword}</h3>
+        `;
+
+        card.onclick = () => {
+          location.href =
+            "detail.html?title=" + encodeURIComponent(item.keyword);
+        };
+
+        container.appendChild(card);
+      });
+    })
+    .catch(err => {
+      container.innerHTML =
+        "<p style='color:red'>Gagal load data trending</p>";
+      console.error(err);
+    });
+});
